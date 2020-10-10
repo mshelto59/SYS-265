d971229f20fe5101d2b9a1dab403bf98541fd307
Matthew Shelto <matthew.shelto@mymail.champlain.edu>
2020-10-10 16:27:57 -0400

    worked on script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -2,3 +2,15 @@
 useradd $1
 usermod -aG wheel $1
 
+wget https://github.com/mshelto59/SYS-265/blob/master/linux/public-keys/id_rsa.pub
+
+mkdir -p ~/.ssh
+touch ~/.ssh/authorized_keys
+cat ./id_rsa.pub >> ~/.ssh/authorized_keys
+
+rm id_rsa.pub
+
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin/PermitRootLogin/' /etc/ssh/sshd_config
+
+
